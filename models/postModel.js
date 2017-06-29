453856ffa5fdd5e84fb58e65e2e37cf6c9eacc7b
Masashi Morita <masashi.morita@itdcanada.ca>
2017-06-29 21:29:03 +0900

    :sparkles: Implement model and api for post

---
+++ models/postModel.js
@@ -0,0 +1,37 @@
+'use strict';
+
+let db = require('./db.js');
+
+module.exports = {
+    table: 'masa_posts',
+
+    findById (id) {
+        return db.getRow("select * from " + this.table + " where id=?", [id]);
+    },
+
+    findAll () {
+        return db.getResult("select * from " + this.table + " where is_active=true order by sequence DESC", null);
+    },
+
+    count () {
+        let _self = this;
+        return new Promise((resolve, reject) => {
+            db.query("select COUNT(*) as count from " + _self.table, null, function (error, result) {
+                if (error) reject(error);
+                else resolve(result[0]);
+            });
+        });
+    },
+
+    insert (data) {
+        return db.insertSync(this.table, data);
+    },
+
+    update (id, data) {
+        return db.updateSync(this.table, id, data);
+    },
+
+    delete (id) {
+        return db.updateSync(this.table, id, {is_active: false});
+    }
+};
---
+++ routes/api/v1/posts.js
@@ -0,0 +1,103 @@
+'use strict';
+let express = require('express');
+let router = express.Router();
+let co = require('co');
+let util = require('../../../helper/util.js');
+let postModel = require('../../../models/postModel.js');
+let relationModel = require('../../../models/relationModel.js');
+
+router.get('/', function (req, res) {
+    co(function *() {
+        let posts = (yield postModel.findAll());
+        util.sendResponse(res, 200, posts);
+    }).catch(function (e) {
+        console.log(e);
+        util.sendResponse(res, 500, e.message);
+    });
+});
+
+router.get('/:id', function (req, res) {
+    co(function *() {
+        let id = parseInt(req.params.id);
+        if (!util.isValidId(id)) throw new Error('Invalid ID...');
+        let post = postModel.findById(id);
+        util.sendResponse(res, 200, post);
+    }).catch(function (e) {
+        console.log(e);
+        util.sendResponse(res, 500, e.message);
+    });
+});
+
+router.post('/', function (req, res) {
+    co(function *() {
+        if (!util.isValidId(parseInt(req.cookies.admin))) throw new Error('Admin id is not defined...');
+        if (!util.isValidId(parseInt(req.body.category))) throw new Error('Invalid Category ID...');
+        if (!validateTagId(req.body.tags)) throw new Error('Invalid Tag ID...');
+        let validateResult = validateParams(req.body.post, req.cookies.admin);
+        if (validateResult.error) throw new Error(validateResult.error);
+        let postResult = (yield postModel.insert(validateResult.data));
+        let categoryResult = (yield relationModel.insertPostCategory({post_id: postResult, category_id: req.body.category}));
+        let tagResult = (yield relationModel.insertPostTags(postResult, req.body.tags));
+        util.sendResponse(res, 200, {post: postResult, category: categoryResult, tag: tagResult});
+    }).catch(function (e) {
+        console.log(e);
+        util.sendResponse(res, 500, e.message);
+    });
+});
+
+router.put('/:id', function (req, res) {
+    co(function *() {
+        let id = req.params.id;
+        if (!util.isValidId(id)) throw new Error('Invalid ID...');
+        let result = (yield postModel.update(id, req.body));
+        util.sendResponse(res, 200, result);
+    }).catch(function (e) {
+        console.log(e);
+        util.sendResponse(res, 500, e.message);
+    });
+});
+
+router.delete('/:id', function (req, res) {
+    co(function *() {
+        let id = parseInt(req.params.id);
+        if (id !== parseInt(req.body.id) || !util.isValidId(id)) throw new Error('Invalid ID...');
+        let result = (yield postModel.delete(id));
+        util.sendResponse(res, 200, result);
+    }).catch(function (e) {
+        console.log(e);
+        util.sendResponse(res, 500, e.message);
+    });
+});
+
+function validateParams(params, admin_id) {
+    let error = null;
+    if (params.title === '' || params.title === undefined || params.title === null) error = 'TItle is required...';
+    if (params.image_path === '' || params.image_path === undefined || params.image_path === null) error = 'Cover image is required...';
+    if (params.description === '' || params.description === undefined || params.description === null) error = 'Description is required...';
+    if (params.body === '' || params.body === undefined || params.body === null) error = 'Content body is required...';
+    if (error) return {error: error, data: null};
+
+    let data = {};
+    data.title = params.title;
+    data.image_path = params.image_path;
+    data.description = params.description;
+    data.body = params.body;
+    data.is_active = true;
+    data.created_at = new Date();
+    data.admin_id = admin_id;
+    data.sequence = 0;
+    return {error: null, data: data};
+}
+
+
+function validateTagId(tags) {
+    let length = tags.length;
+    for (let i = 0; i < length; i++) {
+        if (!util.isValidId(parseInt(tags[i]))) {
+            return false;
+        }
+    }
+    return true;
+}
+
+module.exports = router;
