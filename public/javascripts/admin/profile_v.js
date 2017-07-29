a474bb951538f2002aa340cb0591e93a424e36a6
Masashi Morita <masashi.morita@itdcanada.ca>
2017-07-28 17:10:10 -0700

    :sparkles: Implement Profile page

---
+++ public/javascripts/admin/profile_v.js
@@ -0,0 +1,87 @@
+(function ($) {
+    'use strict';
+
+    let Profile = {
+
+        initialize () {
+            let _self = this;
+            this.$form = $('form#profile-form');
+
+            this.$form.on('submit', function (event) {
+                event.preventDefault();
+                let updatedData = {};
+                updatedData.username = $('#profile-username').val().trim();
+                updatedData.location = $('#profile-location').val().trim();
+                updatedData.weather_api = $('#profile-api').val().trim();
+                if (updatedData.username === '') return _self.showError('Username is required...');
+                if (updatedData.location === '') return _self.showError('Location is required...');
+                if (updatedData.weather_api === '') return _self.showError('Weather API Key is required...');
+                $('span.error-text').text('');
+
+                $.ajax({
+                    url: '/api/v1/users',
+                    type: 'PUT',
+                    dataType: 'json',
+                    data: updatedData,
+                    timeout: 10000,
+
+                    success (data, status, errorThrown) {
+                        $('div.admin-user h3').text(updatedData.username);
+                        let success = $('#success-dialog');
+                        success.text('The profile successfully has been updated');
+                        success.fadeIn(1000).delay(3000).fadeOut(1000);
+                    },
+                    error (data, status, errorThrown) {
+                        let error = $('#error-dialog');
+                        error.text('Error occurred : ' + errorThrown);
+                        error.fadeIn(1000).delay(3000).fadeOut(1000);
+                    }
+                });
+            });
+
+            $('#profile-photo').on('change', function (event) {
+                let data = new FormData();
+                let file = $(this).get(0).files[0];
+                data.append('photo', file);
+                $.ajax({
+                    url: '/api/v1/users/profile',
+                    type: 'PUT',
+                    dataType: 'json',
+                    data: data,
+                    contentType: false,
+                    processData:false,
+                    timeout: 10000,
+
+                    success (data, status, errorThrown) {
+                        let reader = new FileReader();
+                        reader.onload = function (e) {
+                            let image = e.target.result;
+                            $('img.img-profile').attr('src', image);
+                            $('img.img-me').attr('src', image);
+                        };
+                        reader.readAsDataURL(file);
+
+                        let success = $('#success-dialog');
+                        success.text('The profile photo successfully has been updated');
+                        success.fadeIn(1000).delay(3000).fadeOut(1000);
+
+                    },
+                    error (data, status, errorThrown) {
+                        let error = $('#error-dialog');
+                        error.text('Error occurred : ' + errorThrown);
+                        error.fadeIn(1000).delay(3000).fadeOut(1000);
+                    }
+                });
+            });
+        },
+
+        showError (error) {
+            $('span.error-text').text(error);
+            return false;
+        }
+    };
+
+    $(function () {
+        Profile.initialize();
+    });
+}).apply(this, [jQuery]);
--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,9 +1,63 @@
 'use strict';
 let express = require('express');
 let router = express.Router();
+let fs = require('fs');
 let co = require('co');
+let multer = require('multer');
 let util = require('../../../helper/util.js');
 let userModel = require('../../../models/userModel.js');
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/assets/profile');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+let upload = multer({storage: storage}).single('photo');
+
+router.put('/', function (req, res) {
+    co(function *() {
+        let id = parseInt(req.cookies.user.id);
+        if (!util.isValidId(id)) throw new Error('Invalid user ID...');
+        let data = req.body;
+        if (data.username === undefined || data.username === '') throw new Error('Username is required...');
+        if (data.location === undefined || data.location === '') throw new Error('Location is reequired...');
+        if (data.weather_api === undefined || data.weather_api === '') throw new Error('Weather API Key is required...');
+        let result = (yield userModel.update(id, data));
+        req.cookies.user.username = data.username;
+        req.cookies.user.location = data.location;
+        req.cookies.user.weather_api = data.weather_api;
+        res.cookie('user', req.cookies.user);
+        res.clearCookie('weather', null);
+        util.sendResponse(res, 200, result);
+    }).catch(function (e) {
+        util.sendResponse(res, 500, e.message);
+        console.log(e);
+    });
+});
+
+router.put('/profile', function (req, res) {
+    upload(req, res, function (error) {
+        co(function *() {
+            let id = parseInt(req.cookies.user.id);
+            if (!util.isValidId(id)) throw new Error('Invalid user ID...');
+            if (error) throw new Error(error.message);
+            let imagePath = util.getImagePath(req.file);
+            if (imagePath === null) throw new Error('Profile Photo is required...');
+            let result = (yield userModel.update(id, {image_path: imagePath}));
+            if (req.cookies.user.image_path !== null) {
+                fs.unlink('public/assets/profile/' + req.cookies.user.image_path);
+            }
+            req.cookies.user.image_path = imagePath;
+            res.cookie('user', req.cookies.user);
+            util.sendResponse(res, 200, result);
+        }).catch(function (e) {
+            util.sendResponse(res, 500, e.message);
+            console.log(e);
+        });
+    });
+});
 
 router.put('/resetPassword', util.allowAction, function (req, res) {
     co(function *() {
