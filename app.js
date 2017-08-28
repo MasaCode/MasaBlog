07a32bb6c6dc7f64522f0437972824f7eccf0d84
Masashi Morita <masashi.morita@itdcanada.ca>
2017-08-28 12:13:18 -0700

    :recycle: Merge server set up code into app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let debug = require('debug')('masablog:server');
+let http = require('http');
 let express = require('express');
 let session = require('express-session');
 let path = require('path');
@@ -25,6 +27,10 @@ let db = require('./models/db');
 db.connect();
 
 let app = express();
+
+let port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+let server = http.createServer(app);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -61,6 +67,61 @@ app.use('/api/v1/users', users);
 app.use('/api/v1/messages', messages);
 app.use('/api/v1/comments', comments);
 
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function normalizePort(val) {
+    let port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
+
+
+// Event listener for HTTP server "error" event.
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    let bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+// Event listener for HTTP server "listening" event.
+function onListening() {
+    let addr = server.address();
+    let bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+}
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     let err = new Error('Not Found');
@@ -95,4 +156,3 @@ fs.stat('public/assets/profile', function (err, data) {
     }
 });
 
-module.exports = app;
